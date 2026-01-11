c5a9a69b09cc5ce6b8b48b09aad900986ac9dc70
John Ameh <johnameh29@gmail.com>
2026-01-11 02:33:41 +0100

    quiz_payment.php

--- details/quiz_payment.php
+++ details/quiz_payment.php
@@ -138,8 +138,7 @@ if ($result->num_rows > 0) {
 
         function payWithPaystack() {
             const handler = PaystackPop.setup({
-                key: 'pk_test_fdeb97ce15dc119e28cc589fcb24fac669b14f81',
-                // key: 'pk_live_4f399bf20785fe69402e909561b671795972e56c',
+                key: '',
                 email: email,
                 amount: amount,
                 currency: "NGN",
